b8dce802e0972622ba3335fd15fc20fbf71a2018
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2022-07-19 23:31:17 +0200

    fetch notifications

--- src/App.js
+++ src/App.js
@@ -1,28 +1,82 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { uiActions } from "./store/uiSlice";
 import { useEffect } from "react";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const notification = useSelector((state) => state.ui.notification);
+
+  const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    if (cart.items.length > 0) {
-      fetch(
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
         "https://udemy-max-react-http-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
         { method: "PUT", body: JSON.stringify(cart) }
       );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      // const responseData = await response.json();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
     }
-  }, [cart]);
+
+    sendCartData().catch((error) => {
+      const errorMessage = error.message;
+
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: errorMessage,
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/store/uiSlice.js
+++ src/store/uiSlice.js
@@ -2,13 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false },
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggleCart: (state) => {
       state.cartIsVisible = !state.cartIsVisible;
     },
     openCart: (state) => {
       state.cartIsVisible = true;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
