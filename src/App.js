13ebb45a41e22fc5ff978e4f6544ec72d866450d
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2022-07-24 23:57:34 +0200

    added fetching cart state from Firebase with thunk

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
-import { uiActions } from "./store/uiSlice";
 import { useEffect } from "react";
 import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cartActions";
 
 let isInitial = true;
 
@@ -17,50 +17,18 @@ function App() {
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://udemy-max-react-http-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      // const responseData = await response.json();
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      const errorMessage = error.message;
-
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: errorMessage,
-        })
-      );
-    });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
---
+++ src/store/cartActions.js
@@ -0,0 +1,92 @@
+import { uiActions } from "./uiSlice";
+import { cartActions } from "./cartSlice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://udemy-max-react-http-default-rtdb.europe-west1.firebasedatabase.app/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data from server!");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      const errorMessage = error.message;
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: errorMessage,
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://udemy-max-react-http-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      // const responseData = await response.json();
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      const errorMessage = error.message;
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: errorMessage,
+        })
+      );
+    }
+  };
+};
